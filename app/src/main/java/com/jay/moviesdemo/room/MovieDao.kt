fbd30e739d7fcac890c4c567d3883462ad6b014c
Fatiya, Jayantilal Vardaram (US - Mumbai) <jfatiya@deloitte.com>
2020-10-23 18:44:46 +0530

    Added UI for Search Movie

--- app/src/main/java/com/jay/moviesdemo/room/MovieDao.kt
+++ app/src/main/java/com/jay/moviesdemo/room/MovieDao.kt
@@ -3,6 +3,7 @@ package com.jay.moviesdemo.room
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import androidx.room.Query
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.jay.moviesdemo.model.Movie
 
 @Dao
@@ -13,5 +14,10 @@ interface MovieDao {
 
     @Query("SELECT * FROM movies_table")
     fun getMovies(): LiveData<List<Movie>>
+
+    @RawQuery(observedEntities = [Movie::class])
+    fun getFilteredMovies(
+        query: SupportSQLiteQuery
+    ) : LiveData<List<Movie>>
 
 }
